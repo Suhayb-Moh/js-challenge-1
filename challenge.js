6f30d276005cc46de4ed16a5c28e898bbf8bb229
Suhayb Mohamed <suhaybmaxamed@gmail.com>
2022-04-03 08:56:38 +0300

    done the challenge

--- challenge.js
+++ challenge.js
@@ -1,2 +1,20 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+// Mohamed BMI
+let MohamedHigherBMI;
+const mohamedMass = 78;
+const mohamedHeight = 1.69;
+const MohamedBMI = Math.floor(mohamedMass / mohamedHeight ** 2);
+console.log(MohamedBMI);
+
+// Ali BMI
+let AliHigherBMI;
+const aliMass = 92;
+const AliHeight = 1.95;
+const AliBMI = Math.floor(aliMass / AliHeight ** 2);
+console.log(AliBMI);
+
+// Compare between AliBMI and MohamedBMI
+MohamedHigherBMI = MohamedBMI > AliBMI;
+console.log(MohamedHigherBMI);
